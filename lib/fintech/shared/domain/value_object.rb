0d0cd91ef6da444a7b3a75c249e6e423e0984ae2
David Montesdeoca <backpackerhh@gmail.com>
2023-11-20 11:22:43 +0100

    Allow to raise custom domain exceptions for value objects
    References:
    
    * https://github.com/dry-rb/dry-struct/blob/release-1.6/lib/dry/struct/class_interface.rb#L249

--- lib/fintech/shared/domain/value_object.rb
+++ lib/fintech/shared/domain/value_object.rb
@@ -8,6 +8,13 @@ module Fintech
       class ValueObject < Dry::Struct
         transform_keys(&:to_sym)
 
+        # @see https://github.com/dry-rb/dry-struct/blob/release-1.6/lib/dry/struct/class_interface.rb#L249
+        def self.new(*)
+          super
+        rescue Dry::Struct::Error => e
+          raise InvalidArgumentError, e
+        end
+
         def self.value_type(type)
           attribute :value, type
         end
