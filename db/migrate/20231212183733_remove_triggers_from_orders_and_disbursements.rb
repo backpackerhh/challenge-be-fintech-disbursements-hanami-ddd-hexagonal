73c5ae8a23c21bef342f1559b9a9eda05b80fc0d
David Montesdeoca <backpackerhh@gmail.com>
2023-12-12 19:42:56 +0100

    Remove triggers to update orders when disbursements are created or updated
    Instead of entrust the database with that responsibility, being so
    coupled to it, now a domain event is published notifying about a created
    disbursement and eventually all related orders will be properly updated
    (eventual consistency).
    
    References:
    
    * https://www.postgresql.org/docs/current/sql-createfunction.html
    * https://www.postgresql.org/docs/current/sql-createtrigger.html

---
+++ db/migrate/20231212183733_remove_triggers_from_orders_and_disbursements.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+ROM::SQL.migration do
+  up do
+    execute <<~SQL
+      DROP TRIGGER IF EXISTS disbursement_update_trigger ON public.disbursements;
+      DROP FUNCTION IF EXISTS update_disbursement_id() CASCADE;
+    SQL
+  end
+
+  down do
+    execute <<~SQL
+      CREATE OR REPLACE FUNCTION update_disbursement_id()
+      RETURNS TRIGGER AS $$
+      BEGIN
+          UPDATE public.orders
+          SET disbursement_id = NULL
+          WHERE id = ANY(OLD.order_ids);
+
+          UPDATE public.orders
+          SET disbursement_id = NEW.id
+          WHERE id = ANY(NEW.order_ids);
+
+          RETURN NEW;
+      END;
+      $$ LANGUAGE plpgsql;
+
+      CREATE TRIGGER disbursement_update_trigger
+      AFTER INSERT OR UPDATE OF order_ids
+      ON public.disbursements
+      FOR EACH ROW
+      EXECUTE FUNCTION update_disbursement_id();
+    SQL
+  end
+end
