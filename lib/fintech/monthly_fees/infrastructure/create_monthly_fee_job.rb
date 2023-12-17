e124cdecd6d6725aef7b7ed25aacdfae2d160a0e
David Montesdeoca <backpackerhh@gmail.com>
2023-12-17 18:36:31 +0100

    Add logic to job in charge of creating monthly fees if necessary
    A monthly fee must be created only if the total commissions amount is
    less than the minimum monthly fee defined for the merchant.
    
    The acceptance (or e2e) test is now green.

--- lib/fintech/monthly_fees/infrastructure/create_monthly_fee_job.rb
+++ lib/fintech/monthly_fees/infrastructure/create_monthly_fee_job.rb
@@ -6,8 +6,30 @@ module Fintech
       class CreateMonthlyFeeJob < Shared::Infrastructure::Job
         sidekiq_options queue: "disbursements", unique: true, retry_for: 3600 # 1 hour
 
+        include Deps[find_merchant_use_case: "merchants.find.use_case",
+                     find_monthly_order_commissions_use_case: "order_commissions.find_monthly.use_case"]
+
         def perform(merchant_id, start_date)
-          # TODO
+          merchant = find_merchant_use_case.find(merchant_id)
+          previous_month = Date.parse(start_date).prev_month
+          formatted_previous_month = previous_month.strftime("%Y-%m")
+          commissions_amount = find_monthly_order_commissions_use_case.sum_monthly_amount(
+            merchant_id:,
+            beginning_of_month: Date.new(previous_month.year, previous_month.month, 1)
+          )
+
+          if merchant.minimum_monthly_fee.value > commissions_amount
+            Application::CreateMonthlyFeeUseCase.new.create(
+              merchant_id:,
+              commissions_amount:,
+              amount: merchant.minimum_monthly_fee.value - commissions_amount,
+              month: formatted_previous_month
+            )
+
+            logger.info("Monthly fee for merchant #{merchant_id} successfully created (#{formatted_previous_month})")
+          else
+            logger.info("No monthly fee created for merchant #{merchant_id} (#{formatted_previous_month})")
+          end
         end
       end
     end
